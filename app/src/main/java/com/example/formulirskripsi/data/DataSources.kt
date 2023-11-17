498f6238c8326f19dcd4fad4e5dd18ce2fb38f75
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-11-17 13:46:58 +0700

    Create data sources

---
+++ app/src/main/java/com/example/formulirskripsi/data/DataSources.kt
@@ -0,0 +1,11 @@
+package com.example.formulirskripsi.data
+
+import com.example.formulirskripsi.R
+
+object DataSources {
+    val dosbing = listOf(
+        R.string.p_haris,
+        R.string.p_joko,
+        R.string.b_april
+    )
+}
