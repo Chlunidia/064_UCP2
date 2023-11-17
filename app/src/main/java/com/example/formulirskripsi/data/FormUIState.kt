448c4c6a5afe0e458561a418a869fe9dab0b77c3
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-11-17 13:49:18 +0700

    Create data class

---
+++ app/src/main/java/com/example/formulirskripsi/data/FormUIState.kt
@@ -0,0 +1,10 @@
+package com.example.formulirskripsi.data
+
+data class FormUIState(
+    val name: String = "",
+    val nim: String = "",
+    val konsen: String = "",
+    val judul: String = "",
+    val dosbing1: String = "",
+    val dosbing2: String = ""
+)
